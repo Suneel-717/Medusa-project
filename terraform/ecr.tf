4ee9f649d0cdae5c38abea5d57201058a2d7f98c
Thimmampallisunil <thimmampallisunil717@gmail.com>
2025-07-18 21:08:56 +0530

    frist commit

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,9 +1,6 @@
 resource "aws_ecr_repository" "my_repo" {
   name = "my_repo"
-}
-resource "aws_ecr_repository" "my_repo" {
-  name = "my-repo"
-
+  
   image_scanning_configuration {
     scan_on_push = true
   }
