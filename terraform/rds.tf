b773d85624715615268fb1cf146619c973140505
Thimmampallisunil <thimmampallisunil717@gmail.com>
2025-07-18 16:07:38 +0530

    terraform

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -4,12 +4,11 @@ resource "aws_db_subnet_group" "medusa_subnet" {
 }
 
 resource "aws_rds_cluster" "medusa_db" {
-  cluster_identifier = "medusa-db"
-  engine             = "aurora-postgresql"
-  master_username    = var.db_username
-  master_password    = var.db_password
-  database_name      = var.db_name
-  db_subnet_group_name = aws_db_subnet_group.medusa_subnet.name
-
-  vpc_security_group_ids = [aws_security_group.ecs_sg.id]
+  cluster_identifier      = "medusa-db"
+  engine                  = "aurora-postgresql"
+  master_username         = admin   # Use medusauser
+  master_password         = admin  # You can keep this as "admin" (but consider stronger passwords)
+  database_name           = medusadb
+  db_subnet_group_name    = aws_db_subnet_group.medusa_subnet.name
+  vpc_security_group_ids  = [aws_security_group.ecs_sg.id]
 }
