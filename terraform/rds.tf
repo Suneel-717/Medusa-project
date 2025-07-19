18f105cd4605fd63790c14d31b207c783f6dfcde
Thimmampallisunil <thimmampallisunil717@gmail.com>
2025-07-19 09:04:07 +0530

    frist commit

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,5 +1,5 @@
-resource "aws_db_subnet_group" "medusa_subnet" {
-  name       = "medusa-db-subnet"
+resource "aws_db_subnet_group" "medusaa_subnet" {
+  name       = "medusa-db-subnetv2"
   subnet_ids = aws_subnet.public[*].id
 }
 
@@ -9,6 +9,8 @@ resource "aws_rds_cluster" "medusa_db" {
   master_username         = "admin123"   
   master_password         = "admin717"
   database_name           = "medusadb"
-  db_subnet_group_name    = aws_db_subnet_group.medusa_subnet.name
+  db_subnet_group_name    = aws_db_subnet_group.medusaa_subnet.name
   vpc_security_group_ids  = [aws_security_group.ecs_sg.id]
+  
+  skip_final_snapshot     = true
 }
